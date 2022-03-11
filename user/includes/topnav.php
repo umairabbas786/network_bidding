e22d92c27f26992915a3d0c839df0746652a9701
Umair Abbas <umairabbass786@gmail.com>
2022-03-12 04:05:26 +0500

    user panel design setup

---
+++ user/includes/topnav.php
@@ -0,0 +1,28 @@
+<div class="container-fluid no-gutters">
+            <div class="row">
+                <div class="col-lg-12 p-0 ">
+                    <div class="header_iner d-flex justify-content-between align-items-center">
+                        <div class="sidebar_icon d-lg-none">
+                            <i class="ti-menu"></i>
+                        </div>
+                        <div class="serach_field-area d-flex align-items-center"></div>
+                        <div class="header_right d-flex justify-content-between align-items-center">
+                            <div class="profile_info">
+                                <img src="img/profile.png" alt="#">
+                                <div class="profile_info_iner">
+                                    <div class="profile_author_name">
+                                        <h5>Full Name</h5>
+                                        <p>Email</p>
+                                    </div>
+                                    <div class="profile_info_details">
+                                        <a href="profile.php">My Profile </a>
+                                        <a href="setting.php">Settings</a>
+                                        <a href="logout.php">Log Out </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
---
+++ user/setting.php
@@ -0,0 +1,15 @@
+<?php include "includes/header.php";?>
+<?php include "includes/sidenav.php";?>
+<?php include "includes/topnav.php";?>
+<!--Main Work-->
+<div class="main_content_iner ">
+    <div class="container-fluid p-0 ">
+        <div class="row ">
+            main working
+        </div>
+    </div>
+</div>
+<!--Main Work-->
+<?php include "includes/footer.php";?>
+<?php include "includes/script.php";?>
+6C63FF
